1bd2fbd42d773e4f12c53da20473c8ccfa2efcf8
kais-chalghoumi <kais.chalghoumi@esprit.tn>
2023-08-09 00:40:37 +0100

    controllers

---
+++ BACKEND/src/main/java/tn/nat/cnss/gestion_allocations_familiales/controllers/Vue11adController.java
@@ -0,0 +1,11 @@
+package tn.nat.cnss.gestion_allocations_familiales.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/Vue11ad")
+public class Vue11adController {
+}
